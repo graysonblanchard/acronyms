df8a9c99f43aa8e2da67235e6bb35d0af54a0e26
Grayson <glb002@mcdaniel.edu>
2024-10-16 16:37:19 -0400

    More css, add stars.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,43 +3,155 @@ import Keyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
 
 export function Board() {
+  const clue = "JFMAMJJASON";
+  const solutionLetter = "D";
+  //const solutionWord = "december";
+  const solutionExplanation = "the months of the year";
+
   //const [clue, setClue] = React.useState("AAADKTD");
   //const [solutionLetter, setSolutionLetter] = React.useState("A");
   //const [solutionWord, setSolutionWord] = React.useState("away");
 
   const [input, setInput] = React.useState("");
   const [showSubmit, setShowSubmit] = React.useState(false);
+  const [livesLeft, setLivesLeft] = React.useState(3);
+
+  const getStars = () => {
+    switch (livesLeft) {
+      case 3:
+        return (
+          <>
+            <svg
+              viewBox="0 0 1024 1024"
+              fill="currentColor"
+              height="1.5em"
+              width="1.5em"
+            >
+              <path
+                fill="gold"
+                d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"
+              />
+            </svg>
+            <svg
+              viewBox="0 0 1024 1024"
+              fill="currentColor"
+              height="1.5em"
+              width="1.5em"
+            >
+              <path
+                fill="gold"
+                d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"
+              />
+            </svg>
+            <svg
+              viewBox="0 0 1024 1024"
+              fill="currentColor"
+              height="1.5em"
+              width="1.5em"
+            >
+              <path
+                fill="gold"
+                d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"
+              />
+            </svg>
+          </>
+        );
+      case 2:
+        break;
+      case 1:
+        break;
+      default:
+        return (
+          <>
+            <svg
+              viewBox="0 0 1024 1024"
+              fill="currentColor"
+              height="1.5em"
+              width="1.5em"
+            >
+              <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3zM664.8 561.6l36.1 210.3L512 672.7 323.1 772l36.1-210.3-152.8-149L417.6 382 512 190.7 606.4 382l211.2 30.7-152.8 148.9z" />
+            </svg>
+            <svg
+              viewBox="0 0 1024 1024"
+              fill="currentColor"
+              height="1.5em"
+              width="1.5em"
+            >
+              <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3zM664.8 561.6l36.1 210.3L512 672.7 323.1 772l36.1-210.3-152.8-149L417.6 382 512 190.7 606.4 382l211.2 30.7-152.8 148.9z" />
+            </svg>
+            <svg
+              viewBox="0 0 1024 1024"
+              fill="currentColor"
+              height="1.5em"
+              width="1.5em"
+            >
+              <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 00.6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0046.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3zM664.8 561.6l36.1 210.3L512 672.7 323.1 772l36.1-210.3-152.8-149L417.6 382 512 190.7 606.4 382l211.2 30.7-152.8 148.9z" />
+            </svg>
+          </>
+        );
+    }
+    return null;
+  };
+
+  const makeGuess = () => {
+    if (input.toUpperCase() === solutionLetter.toUpperCase()) {
+      window.alert(
+        "Correct! These letters represent " + solutionExplanation + "."
+      );
+    } else {
+      if (livesLeft === 1) {
+        setLivesLeft(livesLeft - 1);
+        window.alert(
+          "Incorrect! The correct answer is " +
+            solutionLetter +
+            ". These letters represent " +
+            solutionExplanation +
+            "."
+        );
+      } else {
+        setLivesLeft(livesLeft - 1);
+        window.alert("Incorrect! Try again.");
+      }
+    }
+  };
 
   return (
     <div>
       <div className="header">ACRONYMS</div>
-      <div className="board">
-        <span className="clue">{"AAADKTD"}</span>
-        <input value={input.toUpperCase()} readOnly />
-        {showSubmit && (
+      <div className="flex-container">
+        <div className="board">
+          <span className="clue">{clue}</span>
+          <input value={input.toUpperCase()} readOnly />
+        </div>
+        <div className="flex-container-2">
           <div className="buttonBar">
-            <button className="submit">Submit</button>
+            {showSubmit && (
+              <button className="submit" onClick={makeGuess}>
+                Submit
+              </button>
+            )}
           </div>
-        )}
-      </div>
-      <div className="keyboard">
-        <Keyboard
-          onChange={(text) => {
-            setInput(text);
-            setShowSubmit(text.length > 0)
-          }}
-          maxLength={1}
-          display={{
-            "{bksp}": " ⌫ ",
-          }}
-          layout={{
-            default: [
-              "q w e r t y u i o p",
-              "a s d f g h j k l",
-              "z x c v b n m {bksp}",
-            ],
-          }}
-        />
+          <div>{getStars()}</div>
+        </div>
+        <div className="keyboard">
+          <Keyboard
+            onChange={(text) => {
+              setInput(text);
+              setShowSubmit(text.length > 0);
+            }}
+            maxLength={1}
+            display={{
+              "{bksp}": " ⌫ ",
+            }}
+            layout={{
+              default: [
+                "q w e r t y u i o p",
+                "a s d f g h j k l",
+                "z x c v b n m {bksp}",
+              ],
+            }}
+          />
+        </div>
       </div>
     </div>
   );
