0de8255eeb77596579eb06acbeb4452d7bd9a1c0
Grayson Blanchard <blanchardg@hhmi.org>
2024-10-18 17:20:31 -0400

    Another test for Safari.

--- src/components/clues.ts
+++ src/components/clues.ts
@@ -8,7 +8,7 @@ export interface Clue {
 
 export const ClueError: Clue = {
     clue: "ERROR",
-    clueDate: new Date("01-01-2024"),
+    clueDate: new Date("01/01/2024"),
     solutionLetter: "E",
     solutionWord: "ERROR",
     solutionExplanation: "ERROR"
@@ -17,21 +17,21 @@ export const ClueError: Clue = {
 export const CluesList: Clue[] = [
     {
         clue: "JFMAMJJASON",
-        clueDate: new Date("10-18-2024"),
+        clueDate: new Date("10/18/2024"),
         solutionLetter: "D",
         solutionWord: "DECEMBER",
         solutionExplanation: "the months of the year"
     },
     {
         clue: "TESTTWO",
-        clueDate: new Date("10-19-2924"),
+        clueDate: new Date("10/19/2924"),
         solutionLetter: "D2",
         solutionWord: "DECEMBER2",
         solutionExplanation: "the months of the year2"
     },
     {
         clue: "TESTTHREE",
-        clueDate: new Date("10-20-2024"),
+        clueDate: new Date("10/20/2024"),
         solutionLetter: "D3",
         solutionWord: "DECEMBER3",
         solutionExplanation: "the months of the year3"
